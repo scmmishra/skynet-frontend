6ae426a008af2f31b7459b59e9cc6a7b249487f0
Shivam Mishra <scm.mymail@gmail.com>
2022-11-24 16:33:40 +0530

    fix: remove unused import

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
-import { appendFile } from "fs";
 
 const app = createApp(App);
 
