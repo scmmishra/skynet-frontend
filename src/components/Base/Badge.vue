2ac5cdb642d2e00a18571e94237f62df17f156f7
Shivam Mishra <scm.mymail@gmail.com>
2022-11-24 13:25:48 +0530

    refactor: update badge spacing and colors

--- src/components/Base/Badge.vue
+++ src/components/Base/Badge.vue
@@ -13,22 +13,23 @@ const props = withDefaults(defineProps<Props>(), {
 
 const toneState = computed(() => {
   if (props.tone === 'positive') {
-    return 'bg-green-100 text-green-600'
+    return 'bg-green-100 text-green-700'
   }
 
   if (props.tone === 'negative') {
-    return 'bg-red-100 text-red-600'
+    return 'bg-red-100 text-red-700'
   }
 
   if (props.tone === 'warning') {
-    return 'bg-orange-100 text-orange-600'
+    return 'bg-orange-100 text-orange-700'
   }
 
   return 'bg-black-100 text-black-500'
 })
 </script>
 <template>
-  <span class="inline-flex items-center gap-1 px-2 py-1 text-xs leading-none rounded-md" :class="toneState">
+  <span class="inline-flex items-center gap-1 px-1.5 py-1 text-xs leading-none rounded-md font-medium"
+    :class="toneState">
     <slot />
   </span>
 </template>
