88c9a3cf4956d4829c80f5c5237bc424fdbb6590
Shivam Mishra <scm.mymail@gmail.com>
2022-11-24 17:06:21 +0530

    feat: axios is for kids

---
+++ src/utils/request.ts
@@ -0,0 +1,38 @@
+const API_URL = import.meta.env.VITE_API_URL;
+
+/**
+ * API wrapper to fetch from skynet client
+ *
+ * @param  {string} path
+ * @param  {RequestInit} config
+ * @returns Promise
+ */
+async function http<T>(path: string, config: RequestInit): Promise<T> {
+  const requestPath = `${API_URL}${path}`;
+  const request = new Request(requestPath, {
+    ...config,
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+  });
+  const response = await fetch(request);
+
+  if (!response.ok) {
+    throw new Error(`${response.status}: ${response.statusText}`);
+  }
+
+  // error if there is no body, return empty array
+  return response.json().catch(() => ({}));
+}
+
+/**
+ * GET method around the skynet client
+ *
+ * @param  {string} path
+ * @param  {RequestInit} config?
+ * @returns Promise
+ */
+export async function get<T>(path: string, config?: RequestInit): Promise<T> {
+  const init = { method: "GET", ...config };
+  return await http<T>(path, init);
+}
