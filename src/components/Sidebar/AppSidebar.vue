8eb82e6ef5d61f80599bbb24580d42f76fce2a83
Shivam Mishra <scm.mymail@gmail.com>
2022-11-24 20:04:59 +0530

    chore: format sidebar

--- src/components/Sidebar/AppSidebar.vue
+++ src/components/Sidebar/AppSidebar.vue
@@ -1,34 +1,60 @@
 <script lang="ts" setup>
-import { useRoute } from 'vue-router'
+import { useRoute } from "vue-router";
 
 import Avatar from "../Base/Avatar.vue";
-import { Settings, Gauge, LampFloor, TrendingUp, Library, FileText } from "lucide-vue-next";
+import {
+  Settings,
+  Gauge,
+  LampFloor,
+  TrendingUp,
+  Library,
+  FileText,
+} from "lucide-vue-next";
 import SidebarItem from "./SidebarItem.vue";
 import SidebarGroup from "./SidebarGroup.vue";
 
-const route = useRoute()
+const route = useRoute();
 
-const orgName = "DeepSource"
+const orgName = "DeepSource";
 </script>
 
 <template>
-  <div class="flex flex-col h-screen p-3 pt-5 bg-white border-r border-gray-100 drop-shadow-sm">
+  <div
+    class="flex flex-col h-screen p-3 pt-5 bg-white border-r border-gray-100 drop-shadow-sm"
+  >
     <div class="flex items-center justify-between p-2">
-      <button class="flex items-center p-1 rounded-lg gap-x-2 hover:bg-gray-100">
+      <button
+        class="flex items-center p-1 rounded-lg gap-x-2 hover:bg-gray-100"
+      >
         <div class="w-6 h-6 rounded-lg bg-gradient-to-t bg-black-999 p-1">
           <img src="/deepsource.svg" alt="DeepSource logo" />
         </div>
         <span class="text-sm font-normal leading-none">{{ orgName }}</span>
       </button>
-      <Avatar image="https://panoptic.in/files/sepia.jpeg" name="Shivam Mishra" has-badge class="m-1"></Avatar>
+      <Avatar
+        image="https://panoptic.in/files/sepia.jpeg"
+        name="Shivam Mishra"
+        has-badge
+        class="m-1"
+      ></Avatar>
     </div>
-    <div class="flex flex-col justify-between flex-grow py-5 space-y-8 overflow-y-auto">
+    <div
+      class="flex flex-col justify-between flex-grow py-5 space-y-8 overflow-y-auto"
+    >
       <div class="space-y-8">
         <SidebarGroup title="Insights">
-          <SidebarItem title="Overview" link="/" :active="route.name === 'home'">
+          <SidebarItem
+            title="Overview"
+            link="/"
+            :active="route.name === 'home'"
+          >
             <Gauge class="w-4 h-4" />
           </SidebarItem>
-          <SidebarItem title="Metrics" link="/metrics">
+          <SidebarItem
+            title="Metrics"
+            link="/metrics"
+            :active="route.name === 'metrics'"
+          >
             <TrendingUp class="w-4 h-4" />
           </SidebarItem>
           <SidebarItem title="Routes" link="/routes">
