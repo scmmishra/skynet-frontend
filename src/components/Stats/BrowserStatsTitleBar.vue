d0db83097241ebae0c5beb92d9792b1afa74fd7f
Shivam Mishra <scm.mymail@gmail.com>
2022-11-24 16:53:29 +0530

    fix: alignment

--- src/components/Stats/BrowserStatsTitleBar.vue
+++ src/components/Stats/BrowserStatsTitleBar.vue
@@ -3,7 +3,7 @@
     <div class="col-span-1">
       Browser
     </div>
-    <div class="grid grid-cols-3 gap-2 col-span-2">
+    <div class="grid grid-cols-3 gap-2 col-span-2 text-right">
       <div>FCP</div>
       <div>LCP</div>
       <div>CLS</div>
