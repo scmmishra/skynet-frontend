cc60569fe74a9176bef655bb68cffd50b533070b
Shivam Mishra <scm.mymail@gmail.com>
2022-11-24 18:44:06 +0530

    refactor: error copy

--- src/components/Errors/ServerError.vue
+++ src/components/Errors/ServerError.vue
@@ -7,7 +7,7 @@ import Card from '../Base/Card.vue';
     <div class="max-w-[450px] mx-auto flex items-center gap-8">
       <img src="/error-robot.png" class="h-40 flex-shrink-0" />
       <div class="space-y-2">
-        <h1 class="text-2xl font-bold">Unable to reach the server</h1>
+        <h1 class="text-2xl font-bold">Server unavailable :(</h1>
         <p class="text-black-600">
           We're unable to reach the server, our engineerings are working to fix this.
           Please check back in some time
