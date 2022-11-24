5dbb35a2eb9ea2beeefcf31dbb24e2ecb58007d7
Shivam Mishra <scm.mymail@gmail.com>
2022-11-24 19:39:53 +0530

    fix: styling for browser stat

--- src/components/Stats/BrowserStats.vue
+++ src/components/Stats/BrowserStats.vue
@@ -1,9 +1,9 @@
 <script lang="ts" setup>
-import { computed } from 'vue';
-import { Browsers } from '../../types/browsers';
-import { PerformanceMetrics } from '../../types/metrics';
-import BROWSERS from '../../utils/browsers';
-import { formatUnit, metricStatus } from '../../utils/metrics';
+import { computed } from "vue";
+import { Browsers } from "../../types/browsers";
+import { PerformanceMetrics } from "../../types/metrics";
+import BROWSERS from "../../utils/browsers";
+import { formatUnit, metricStatus } from "../../utils/metrics";
 
 const props = defineProps<{
   browser: Browsers;
@@ -16,31 +16,34 @@ const props = defineProps<{
 function computedScore(name: PerformanceMetrics, value: number) {
   const status = metricStatus(name, value);
 
-  if (status === 'negative') {
-    return 'bg-red-500';
+  if (status === "negative") {
+    return "bg-red-500";
   }
 
-  if (status === 'warning') {
-    return 'bg-orange-500';
+  if (status === "warning") {
+    return "bg-orange-500";
   }
 
-  if (status === 'positive') {
-    return 'bg-green-500';
+  if (status === "positive") {
+    return "bg-green-500";
   }
 }
 
 function textColor(name: PerformanceMetrics, value: number) {
   const status = metricStatus(name, value);
 
-  if (status === 'negative') {
-    return 'text-red-500';
+  if (status === "negative") {
+    return "font-medium";
+  }
+
+  if (status === "warning") {
+    return "font-medium";
   }
 }
 
 const currentBrowser = computed(() => {
   return BROWSERS[props.browser];
 });
-
 </script>
 
 <template>
@@ -51,10 +54,15 @@ const currentBrowser = computed(() => {
     </div>
     <div class="grid grid-cols-3 gap-2 col-span-2">
       <div v-for="stat in stats" class="flex justify-end text-base">
-        <div class="text-black-700 inline-flex text-right items-center gap-2 justify-end px-2 py-1 rounded-md"
-          :class="textColor(stat.name, stat.value)">
-          <div v-if="computedScore(stat.name, stat.value)" :class="computedScore(stat.name, stat.value)"
-            class="w-2 h-2 rounded-full" />
+        <div
+          class="text-black-700 inline-flex text-right items-center gap-2 justify-end px-2 py-1 rounded-md tabular-nums"
+          :class="textColor(stat.name, stat.value)"
+        >
+          <div
+            v-if="computedScore(stat.name, stat.value)"
+            :class="computedScore(stat.name, stat.value)"
+            class="w-2 h-2 rounded-full"
+          />
           {{ formatUnit(stat.name, stat.value) }}
         </div>
       </div>
